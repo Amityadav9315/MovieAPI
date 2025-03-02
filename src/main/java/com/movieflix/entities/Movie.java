f85cadab30b273bd3cd6942f844924f4627bd714
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-02 12:34:29 +0530

    fankfad

--- src/main/java/com/movieflix/entities/Movie.java
+++ src/main/java/com/movieflix/entities/Movie.java
@@ -1,17 +1,21 @@
 package com.movieflix.entities;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Set;
 
 @Entity
 public class Movie {
  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    @Column(nullable=false,length = 200)
+    @NotBlank(message = "Please provide movie ticket ")
     private  Integer movieId;
-    
-    
+
     private String title;
     
     private String director;
@@ -20,7 +24,7 @@ public class Movie {
     
     private Set<String> movieCaste;
 
-    private integer releaseyear;
+    private Integer releaseYear;
 
 
 
