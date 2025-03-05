b9b54c325141941828f48a7dea617f86d829c7e9
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-06 00:46:58 +0530

    dxwnlwftry

--- src/main/java/com/movieflix/MovieApiApplication.java
+++ src/main/java/com/movieflix/MovieApiApplication.java
@@ -2,9 +2,11 @@ package com.movieflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class MovieApiApplication {
+	GsonBuilderUtils
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
