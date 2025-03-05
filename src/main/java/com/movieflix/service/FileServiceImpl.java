d408b01421c1a7d8de44b310a8fd51db84b1dde7
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-06 01:37:47 +0530

    dsbsdfblwef

--- src/main/java/com/movieflix/service/FileServiceImpl.java
+++ src/main/java/com/movieflix/service/FileServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,6 +14,11 @@ public class FileServiceImpl implements  FileService{
 
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
+       //get name of the file
+        String fileName=file.getOriginalFilename();
+        // to get the file path
+        String filePath=path+ File.separator+fileName;
+
         return "";
     }
 
