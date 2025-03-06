3c01fc66b2231d0f5c3d96668cfeaca9cca6ef69
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-06 17:39:37 +0530

    fgrteyusj

--- src/main/java/com/movieflix/controllers/FileControllers.java
+++ src/main/java/com/movieflix/controllers/FileControllers.java
@@ -2,15 +2,16 @@ package com.movieflix.controllers;
 
 
 import com.movieflix.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/file/")
@@ -28,6 +29,14 @@ public class FileControllers {
          String  uploadedFileName=fileService.uploadFile(path,file);
          return ResponseEntity.ok("File uploaded succesfully"+ uploadedFileName);
 
+
+     }
+     @GetMapping("/{fileName}")
+     public void serverFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException{
+
+         InputStream resourceFile=fileService.getResourceFile(path,fileName);
+         response.setContentType(MediaType.IMAGE_GIF_VALUE);
+         StreamUtils.copy(resourceFile,response.getOutputStream());
 
      }
 
